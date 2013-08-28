c4340660dcac6e3e9e4eeff8003a91102f9cb540
Sam Podlogar <sam@podlogar.com>
2013-08-27 20:30:21 -0500

    Added the framework
    Added the Komodo Mobile framework files to repository

---
+++ Komodo_mobile/Komodo_Mobile.js
@@ -0,0 +1,44 @@
+$(document).ready( function(e){
+	if($("div[data-komodo=page]").attr("data-komodo") == "page"){
+		$("div[data-komodo=page]").addClass("komodo_page");
+	}
+	if($("div[data-komodo=head]").attr("data-komodo") == "head"){
+		$("div[data-komodo=head]").addClass("komodo_head");
+	}
+	if($("div[data-komodo=content]").attr("data-komodo") == "content"){
+		$("div[data-komodo=content]").addClass("komodo_content");
+	}
+	if($("div[data-komodo=innercontent]").attr("data-komodo") == "innercontent"){
+		$("div[data-komodo=innercontent]").addClass("komodo_inner_content");
+	}
+	if($("div[data-komodo=list_title]").attr("data-komodo") == "list_title"){
+		$("div[data-komodo=list_title]").addClass("komodo_list_title");
+	}
+	if($("ul[data-komodo=link_list]").attr("data-komodo") == "link_list"){
+		$("ul[data-komodo=link_list]").addClass("komodo_link_list");
+	}
+	if($("ul[data-komodo=list]").attr("data-komodo") == "list"){
+		$("ul[data-komodo=list]").addClass("komodo_list");
+	}
+	if($("[data-komodo=list_count]").attr("data-komodo") == "list_count"){
+		$("[data-komodo=list_count]").addClass("komodo_list_count");
+	}
+	if($("span[data-komodo=lightText]").attr("data-komodo") == "lightText"){
+		$("span[data-komodo=lightText]").addClass("komodo_lightText");
+	}
+	if($("div[data-komodo=foot]").attr("data-komodo") == "foot"){
+		$("div[data-komodo=foot]").addClass("komodo_foot");
+	}
+	if($("div[data-komodo=innercontent]").attr("data-komodo") == "innercontent"){
+		$("div[data-komodo=innercontent]").addClass("komodo_inner_content");
+	}
+	if($("div[data-komodo=list_title]").attr("data-komodo") == "list_title"){
+		$("div[data-komodo=list_title]").addClass("komodo_list_title");
+	}
+	if($("div[data-komodo=link_list]").attr("data-komodo") == "link_list"){
+		$("div[data-komodo=link_list]").addClass("komodo_link_list");
+	}
+	if($("ul[data-komodo=list]").attr("data-komodo") == "list"){
+		$("ul[data-komodo=list]").addClass("komodo_list");
+	}
+});
